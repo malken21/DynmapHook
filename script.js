e85c97d4ce6a70270661887be1d1b12ab633be9b
malken21 <Malken_Marumasa@outlook.jp>
2022-09-03 00:17:55 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-function test() {
+
+function Change() {
+    const type = document.srv.add.value;
+    const iframe = document.getElementById("map");
+    iframe.src = type;
+    console.log(type);
 }
